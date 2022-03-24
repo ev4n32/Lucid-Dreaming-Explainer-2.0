7ed11070a2f5326452929ef4e6fe491541ca04ca
Evan <evanwilsongz@gmail.com>
2022-03-24 02:04:09 -0400

    Added date for dreams submitted, made overflow-y for #dreamlist scrollable, and made the dream list show most recent at the top

--- script.js
+++ script.js
@@ -1,48 +1,99 @@
+// Expand Div Function for collapsable buttons
+function expandDiv(a) {
+  var x = document.getElementById(a);
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+// Verifies if email is proper format
+function emailSubmit() {
+  var email = document.forms[0].email.value;
+  if (email.includes("@")){
+      alert ("Thanks for joining! " + email + " has been added to our newsletter");
+  } else {
+      alert  (email + " is not a valid email");
+  }
+}
+//Pantry API URL
 const api_url = 
       "https://getpantry.cloud/apiv1/pantry/045f05da-4502-496e-bf5e-1ffbb280ab75/basket/dreamLogs";
 
-// Defining async function
+// Lists dreams from Pantry API basket
 async function getDream(url) {
-    
-    // Storing response
     const response = await fetch(url);
     
     var data = await response.json();
     console.log(data);
 
-    for (let i = 0; i < data['dreamLog'].length; i++) {
+    // This block displays dreams from oldest to newest
+    // for (let i = 0; i < data['dreamLog'].length; i++) {
+    //   dreamName = data['dreamLog'][i]['name'];
+    //   dream = data['dreamLog'][i]['dream'];
+    //   document.getElementById('dreamlog').innerHTML += `<div id="dream"><b>${dreamName}'s dream:</b> ${dream} <br><sub>${date}</sub></div>`;
+    // }
+
+    // This block displays dreams from newest to oldest
+    for (let i = data['dreamLog'].length - 1; i >= 0; i--) {
       dreamName = data['dreamLog'][i]['name'];
       dream = data['dreamLog'][i]['dream'];
-      document.getElementById('dreamlog').innerHTML += `<div id="dream"><b>${dreamName}'s dream:</b> ${dream}</div>`;
+      date = data['dreamLog'][i]['date'];
+      document.getElementById('dreamlog').innerHTML += `<div id="dream"><b>${dreamName}'s dream:</b> ${dream}<br><sub>${date}</sub></div>`;
     }
 }
+
+// Searches through dreamLog array in Pantry Basket
 async function searchDreams() {
 
   document.getElementById('dreamlog').innerHTML = "";
 
   const response = await fetch(api_url);
-
   const data = await response.json();
   console.log(data);
 
   const searchTerm = document.getElementById('nameSearch').value;
+  let count = 0;
 
-  for (let i = 0; i < data['dreamLog'].length; i++) {
-    dreamName = data['dreamLog'][i]['name'];
-    dream = data['dreamLog'][i]['dream'];
-    
-    if(dreamName === searchTerm) {
-      document.getElementById('dreamlog').innerHTML += `<div id="dream"><b>${dreamName}'s dream:</b> ${dream}</div>`
-    }
+  if (searchTerm === "") {
+    document.getElementById('dreamlog').innerHTML = "No search term entered";
+  } else {
+      for (let i = 0; i < data['dreamLog'].length; i++) {
+        dreamName = data['dreamLog'][i]['name'];
+        dream = data['dreamLog'][i]['dream'];
+        date = data['dreamLog'][i]['date'];
+        
+        if(dreamName === searchTerm) {
+          document.getElementById('dreamlog').innerHTML += `<div id="dream"><b>${dreamName}'s dream:</b> ${dream}<br><sub>${date}</sub></div>`
+          count++
+        } 
+      }
+      if (count === 0){
+        document.getElementById('dreamlog').innerHTML = "No results found"
+      }
   }
+}
+function resetDreams() {
+  document.getElementById('dreamlog').innerHTML = ""
+  getDream(api_url);
 }
 // Calling that async function
 getDream(api_url);
 
+function postDream(){
+  if(document.getElementById('namelog').value === "" || document.getElementById('log').value === "") {
+    alert('You must complete the form to submit a dream');
+  } else {
+    putDream();
+  }
+}
 async function putDream(){
+  const date = new Date();
+  const timestamp = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
   const dreamData = {dreamLog:[{
     name : document.getElementById('namelog').value,
-    dream : document.getElementById('log').value
+    dream : document.getElementById('log').value,
+    date: timestamp
   }]}
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
